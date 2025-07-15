1db5d7c79afad19a6d3da695a27c96ed23908413
Kevin Dos Santos Alves <kevdos-s@student.42lausanne.ch>
2025-07-15 15:39:24 +0200

    fix struct

--- global.h
+++ global.h
@@ -6,7 +6,7 @@
 /*   By: kevdos-s <kevdos-s@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 12:51:16 by kevdos-s          #+#    #+#             */
-/*   Updated: 2025/07/15 13:06:44 by kevdos-s         ###   ########.fr       */
+/*   Updated: 2025/07/15 15:39:14 by kevdos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,18 @@
 # define GLOBAL_H
 
 typedef struct s_point{
-	int line;
-	int column;
+	int row;
+	int col;
 } t_point;
 
 typedef struct s_map{
-	int size;
+	int size_row;
+	int size_col;
 	char empty_char;
 	char obstacle_char;
 	char fill_char;
 	char **map;
 } t_map;
 
-typedef struct s_list_point{
-	t_point *previous;
-	t_point *next;
-	t_point *first;
-	t_point *last;
-	t_point current;
-	int current_index;
-} t_list_point;
 
 #endif
