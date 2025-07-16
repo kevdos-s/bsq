ff4a5f1c900f30bb2ad387904fca7c563255c003
Antonio Pisanello <antonio.pisanello@students.42lausanne.ch>
2025-07-16 20:56:04 +0200

    Norminette: global and arguments headers files

--- includes/arguments.h
+++ includes/arguments.h
@@ -6,14 +6,14 @@
 /*   By: apisanel <apisanel@students.42lausanne.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/14 14:44:17 by apisanel          #+#    #+#             */
-/*   Updated: 2025/07/15 19:47:51 by apisanel         ###   ########.fr       */
+/*   Updated: 2025/07/16 20:55:28 by apisanel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "global.h"
-
 #ifndef ARGUMENTS_H
 # define ARGUMENTS_H
+
+# include "global.h"
 
 char	**get_map_from_std_input(void);
 t_map	*get_map_from_path(char *path);
--- includes/array_helpers.h
+++ includes/array_helpers.h
@@ -6,14 +6,14 @@
 /*   By: apisanel <apisanel@students.42lausanne.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 12:51:16 by apisanel          #+#    #+#             */
-/*   Updated: 2025/07/15 17:30:38 by apisanel         ###   ########.fr       */
+/*   Updated: 2025/07/16 20:55:19 by apisanel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "global.h"
-
 #ifndef ARRAY_HELPERS_H
 # define ARRAY_HELPERS_H
+
+# include "global.h"
 
 int	min(int *args, int size);
 
