6ffa09107a76063b555ca8909c7eb321724f5063
Antonio Pisanello <antonio.pisanello@students.42lausanne.ch>
2025-07-16 20:54:57 +0200

    Norminette: solve.h

--- includes/solve.h
+++ includes/solve.h
@@ -6,14 +6,14 @@
 /*   By: apisanel <apisanel@students.42lausanne.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 12:51:16 by apisanel          #+#    #+#             */
-/*   Updated: 2025/07/16 13:45:17 by apisanel         ###   ########.fr       */
+/*   Updated: 2025/07/16 20:54:22 by apisanel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "global.h"
-
 #ifndef SOLVE_H
 # define SOLVE_H
+
+# include "global.h"
 
 typedef struct s_min_ctx
 {
