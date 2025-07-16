9b0f2b000a8e26ef83ac224da9fa5422ea55ed11
Antonio Pisanello <antonio.pisanello@students.42lausanne.ch>
2025-07-16 20:57:33 +0200

    Norminette: check.h

--- includes/check.h
+++ includes/check.h
@@ -6,14 +6,14 @@
 /*   By: apisanel <apisanel@students.42lausanne.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 12:51:16 by apisanel          #+#    #+#             */
-/*   Updated: 2025/07/15 20:52:06 by apisanel         ###   ########.fr       */
+/*   Updated: 2025/07/16 20:57:14 by apisanel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CHECK_H
 # define CHECK_H
 
-#include "global.h"
+# include "global.h"
 
 void	ft_check_args_n_fill(char **args, int argc, t_map **map);
 void	ft_check_n_fill_content(int file_d, t_map *map);
