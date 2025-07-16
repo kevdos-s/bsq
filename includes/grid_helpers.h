f4cc52609d82c5a9b763fad14aa8d4aa63418c1f
Antonio Pisanello <antonio.pisanello@students.42lausanne.ch>
2025-07-16 20:58:32 +0200

    Norminette: grid_helpers.h

--- includes/grid_helpers.h
+++ includes/grid_helpers.h
@@ -6,15 +6,15 @@
 /*   By: apisanel <apisanel@students.42lausanne.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 12:51:16 by apisanel          #+#    #+#             */
-/*   Updated: 2025/07/16 13:51:01 by apisanel         ###   ########.fr       */
+/*   Updated: 2025/07/16 20:58:15 by apisanel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "global.h"
-#include "solve.h"
+#ifndef GRID_HELPERS_H
+# define GRID_HELPERS_H
 
-#ifndef GRID_HELPER_H
-# define GRID_HELPER_H
+# include "global.h"
+# include "solve.h"
 
 void	init_dp_grid(t_map *map_ctx);
 void	init_dp_ctx(t_dp_ctx **dp_ctx);
